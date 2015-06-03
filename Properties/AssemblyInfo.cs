c9e497359085d2b16a92a50c3a0e22eac868e8c3
tb2johm <tb2johm@gmail.com>
2015-06-03 16:47:02 +0200

    Updated version numbers

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("MMVVM")]
 [assembly: AssemblyDescription("Helper for MVVM pattern")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Markus Johansson")]
 [assembly: AssemblyProduct("MMVVM")]
 [assembly: AssemblyCopyright("Markus Johansson")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyFileVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0")]
+[assembly: AssemblyFileVersion("0.1")]
